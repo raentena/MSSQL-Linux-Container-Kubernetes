5a91e52e40baa4d9255ddf80843d1fc9d3a48091
root <stv707@gmail.com>
2021-04-21 14:17:22 +0800

    v1

--- lab/mod02/lab03/practice-X/step3_docker_restorewwi.sh
+++ lab/mod02/lab03/practice-X/step3_docker_restorewwi.sh
@@ -1,2 +1,6 @@
 sudo docker exec -d mssql\
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Pa$$w0rd2019' -Q 'RESTORE DATABASE WideWorldImp$
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Pa$$w0rd2019' -Q 'CREATE DATABASE WideWorldImporters'
+
+
+sudo docker exec -d mssql\
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Pa$$w0rd2019' -Q 'RESTORE DATABASE WideWorldImporters'
