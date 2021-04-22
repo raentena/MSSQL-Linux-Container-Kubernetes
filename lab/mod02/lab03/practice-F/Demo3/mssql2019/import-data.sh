e78fb692d4058cdf0ce70abc1ce7c2d05c529cce
root <stv707@gmail.com>
2021-04-22 17:18:42 +0800

    v1

--- lab/mod02/lab03/practice-F/Demo3/mssql2019/import-data.sh
+++ lab/mod02/lab03/practice-F/Demo3/mssql2019/import-data.sh
@@ -1,5 +1,8 @@
-#run the setup script to create the DB and the schema in the DB
+#run the setup script to create the DB and the schema only if WideWorldImporters does not exists
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
+
+if [ ! -s /var/opt/mssql/data/WideWorldImporters.mdf ]
+then 
 for i in {1..50};
 do
     /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Pa55w0rd2019' -d master -i /seed/setup.sql
@@ -12,3 +15,4 @@ do
         sleep 1
     fi
 done
+fi
