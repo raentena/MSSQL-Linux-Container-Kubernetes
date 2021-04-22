027c4c2310dfdcfc6eb4c4e4663a830d0f3116c8
root <stv707@gmail.com>
2021-04-22 18:37:47 +0800

    v1

--- lab/mod02/lab03/practice-F/Demo3/webapp/src/routes/index.js
+++ lab/mod02/lab03/practice-F/Demo3/webapp/src/routes/index.js
@@ -20,7 +20,7 @@ const os = require('os');
 
 const pool = mssql.globalConnectionPool;
 
-var myhostname = 'cibai';
+const myhostname = 'cibai';
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
