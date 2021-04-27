37c67e58e83ba55e4adfa6f73da8f3359f9b4f0b
root <stv707@gmail.com>
2021-04-27 21:29:53 +0800

    v1

---
+++ lab/mod02/lab03/practice-F/Demo3/dockerpull.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+functioni list () 
+{
+cat <<EOF 
+node:10.24                   
+mcr.microsoft.com/mssql/server:2019-latest              
+mcr.microsoft.com/mssql/server:latest                   
+mcr.microsoft.com/mssql/server:2017-latest             
+portainer/portainer-ce:latest                   
+mcr.microsoft.com/mssql/server:2017-CU22-ubuntu-16.04  
+mcr.microsoft.com/mssql/server:2017-CU16-ubuntu        
+EOF
+}
+
+
+for i in $(list)
+do 
+
+ echo "docker pull $i"
+
+done
