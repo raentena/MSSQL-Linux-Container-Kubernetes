62cfe0e85437379768e553b8d62ee8856942be33
stv707 <stv707@gmail.com>
2021-04-29 21:52:59 +0800

    v1

--- lab/mod08/lab04/setup_linux.sh
+++ lab/mod08/lab04/setup_linux.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
-
 # Install az, azdata and kubectl 
 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
-
 
 if [ ! -f /etc/yum.repos.d/azure-cli.repo ]
 then 
@@ -38,7 +36,6 @@ then
 curl -o /etc/yum.repos.d/mssql-server.repo https://packages.microsoft.com/config/rhel/8/prodAZDATA.repo
 fi 
 
-
 yum install azure-cli azdata-cli kubectl -y 
 if [ $? -ne 0 ]
 then 
@@ -48,15 +45,12 @@ fi
 
 sleep 1 
 
-clear 
 echo 'Installation of az cli , kubectl and azdata seems OK'
-
 
 for util in az azdata kubectl curl wget
     do
     echo Verifying $util is in path & which $util 1>/dev/null 2>/dev/null || (echo Unable to locate $util && exit 1)
 done
-
 
 TMPSIZE=$(df -h | grep tmplv | awk '{print $2}')
 
